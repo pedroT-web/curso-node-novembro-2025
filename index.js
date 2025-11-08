8a3d1d8ab3f7ff2513d383e4f6505c881e885185
pedro <Pedrotaroco1@gmail.com>
2025-11-07 22:27:47 -0300

    Aprendendo sobre requisições, portas, rotas, pacotes, modulos

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("olá")
+const express = require("express")
+const app = express()
+app.get("/", function (req, res) {
+    res.send("Hello Word")
+})
+
+app.get("/oi", (req, res) => {
+    res.send("Olá mundo")
+})
+
+app.listen(3000)
